65a2c9383da6dfbf04b3ad184029c9a976862fa0
SNEAKERSBLK12 <cord.singleton1530@gmail.com>
2026-01-30 19:20:07 -0600

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -1,1 +1,17 @@
+(function(){
+  const $ = (sel) => document.querySelector(sel);
+
+  // Active nav highlight
+  const path = (location.pathname.split('/').pop() || 'index.html').toLowerCase();
+  document.querySelectorAll('[data-nav]').forEach(a=>{
+    if(a.getAttribute('href').toLowerCase() === path) a.classList.add('active');
+  });
+
+  // Mobile nav (only if you later add hamburger markup)
+  const hamb = $('#hamburger');
+  const links = $('#navlinks');
+  if(hamb && links){
+    hamb.addEventListener('click', ()=> links.classList.toggle('open'));
+  }
+})();
 
